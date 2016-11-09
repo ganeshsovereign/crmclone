19e4a739c68ecd5a617bfec5bc923e04d787a8a2
Herve Prot <herve.prot@tomanage.fr>
2016-11-09 14:11:38 +0100

    Add webcounter modules

---
+++ definitions/modules.js
@@ -0,0 +1,8 @@
+// WebCounter module
+INSTALL('module', 'https://modules.totaljs.com/latest/webcounter.js');
+
+// Request stats module
+INSTALL('module', 'https://modules.totaljs.com/latest/reqstats.js');
+
+// Total.js monitoring
+INSTALL('module', 'https://modules.totaljs.com/latest/monitor.js');
