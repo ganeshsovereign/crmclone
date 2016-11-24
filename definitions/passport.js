43ceaf09f05d318efc869e6ff350768102e35b71
Herve Prot <herve.prot@tomanage.fr>
2016-11-24 16:14:10 +0100

    Fix oauth2

--- definitions/passport.js
+++ definitions/passport.js
@@ -320,6 +320,7 @@ passport.use(new SymeosOAuth2Strategy({
 
 passport.serializeUser(function (user, done) {
     console.log("Passport !!!!!!!!!!!!!!!!!!!!!!!!!!");
+    return done(null, user);
     var UserModel = MODEL('user').Schema;
     var UserGroup = MODEL('userGroup').Schema;
 
