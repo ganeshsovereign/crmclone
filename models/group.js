1289c3ed4cc8facf03cf836b597c57ab614122bb
CharnaceRegis <charnace.regis@laposte.net>
2017-02-12 16:03:35 +0100

    fix

---
+++ models/group.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+		Schema = mongoose.Schema,
+		timestamps = require('mongoose-timestamp');
+
+
+/**
+ * UserGroup Schema
+ */
+var userGroupSchema = new Schema({
+	name: {type: String, unique: true, lowercase: true},
+	_id: String,
+	_createdAt: {type: Date},
+	updatedAt: Date,
+	notes: String,
+	rights: {
+		type: Schema.Types.Mixed
+	},
+	objectifs: {
+		type: Schema.Types.Mixed
+	},
+        isremoved: Boolean
+});
+
+userGroupSchema.plugin(timestamps);
+
+exports.Schema = mongoose.model('group', userGroupSchema, 'UserGroup');
+exports.name = 'group';
