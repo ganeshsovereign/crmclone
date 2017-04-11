a3343d67237040025670a4d771816037946d1eab
Herve Prot <herve.prot@tomanage.fr>
2017-04-11 15:31:22 +0200

    add debug log

--- definitions/eventemitter.js
+++ definitions/eventemitter.js
@@ -3,3 +3,15 @@ EventEmitter = require('eventemitter3');
 var EE = new EventEmitter();
 
 F.functions.EE = EE;
+
+if (F.isDebug) {
+    F.on('request', function(req) {
+        req.$begin = Date.now();
+        req.on('end', function() {
+            if (req.flags)
+                req.res.once('finish', function() {
+                    console.log(req.ip, req.method, req.url, '[' + ((Date.now() - req.$begin)) + 'ms]', req.res.statusCode);
+                });
+        });
+    });
+}
