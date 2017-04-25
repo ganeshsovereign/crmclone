349967002bbb4f82bfc3cc84b48c091cc875a86b
Herve Prot <herve.prot@tomanage.fr>
2017-04-25 14:57:36 +0200

    Add catalogue print product page

---
+++ controllers/print.js
@@ -0,0 +1,10 @@
+exports.install = function() {
+    F.route('/print/product', view_product);
+};
+
+function view_product() {
+    var self = this;
+    self.meta('Product');
+
+    self.view('product');
+}
