1db643b67300abc43f35af063fa30d4dbcba9fc6
Herve Prot <herve.prot@tomanage.fr>
2017-09-06 06:07:57 +0200

    Update import

--- controllers/customer.js
+++ controllers/customer.js
@@ -1456,6 +1456,8 @@ exports.install = function() {
                                                 var req = {};
                                                 req[idx] = data[idx];
 
+                                                return console.log(data);
+
                                                 SocieteModel.findAndUpdate(req, data, { upsert: false, multi: false, new: true }, function(err, doc) {
                                                     if (err)
                                                         return callback(err);
